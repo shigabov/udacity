b2ffc4dcbff348ee411cdd482cc277b11fe1a031
shigabov <shigabov@gmail.com>
2015-07-22 00:55:28 +0300

    some bugs fixed

--- app/src/main/java/com/example/udacity/MainActivity.java
+++ app/src/main/java/com/example/udacity/MainActivity.java
@@ -16,47 +16,26 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     public void justText (View v) {
         switch (v.getId()) {
             case R.id.btn_spotify:
-                Toast.makeText(getApplicationContext(), "This button will lunch SPOTIFY app!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), R.string.spotify, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_scores:
-                Toast.makeText(getApplicationContext(), "This button will lunch SCORES app!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), R.string.scores, Toast.LENGTH_SHORT).show();
                 break;
-
             case R.id.btn_library:
-                Toast.makeText(getApplicationContext(), "This button will lunch LIBRARY app!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), R.string.library, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_bib:
-                Toast.makeText(getApplicationContext(), "This button will lunch BUILD IT BIGGER app!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), R.string.bib, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_xyz:
-                Toast.makeText(getApplicationContext(), "This button will lunch XYZ READER app!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),  R.string.xyz, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_capstone:
-                Toast.makeText(getApplicationContext(), "This button will lunch my own app!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),  R.string.capstone, Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
